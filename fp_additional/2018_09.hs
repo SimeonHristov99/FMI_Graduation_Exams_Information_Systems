79c7290e8803c942c865e253ad2ea7c8e1c4bc46
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-03-25 12:56:17 +0200

    Hide.

--- fp_additional/2018_09.hs
+++
@@ -1,7 +0,0 @@
-main :: IO()
-main = do
-    print $ sumMaxRoots (\ x -> x^3 - x) [ [1,2,3], [-1,0,5], [1,4,-1] ] == -1
-
-selectList l1 l2 = if length l1 >= length l2 then l1 else l2
-
-sumMaxRoots f ll = sum (foldr selectList [] (map (\ l -> [ x | x <- l, f x == 0 ]) ll))
