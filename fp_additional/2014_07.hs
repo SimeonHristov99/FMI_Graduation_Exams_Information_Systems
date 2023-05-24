c78e443cb7a8203db80ee34043c2018a1451d06b
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-05-24 10:43:28 +0300

    Hide.

--- fp_additional/2014_07.hs
+++ fp_additional/2014_07.hs
@@ -9,10 +9,6 @@ main = do
     print $ ((take 4 $ chainMinCompositions (+1))!!2) 2 == 3
     print $ ((take 4 $ chainMinCompositions (+1))!!3) 2 == 2
 
-totalMin fs = foldr1 (\ f1 f2 -> if f1 0 < f2 0 then f1 else f2) fs
+totalMin fs = ???
 
-chainMinCompositions f = [id, f] ++ helper (f, id) (totalMin [f, id]) [2, 1, 0]
- where
-     helper (f1, f2) tM (i:is)
-      | any (\ j -> f1 j /= f2 j) (i:is) = (f1.f2) : helper ((f1.f2), f1) (totalMin [(f1.f2), tM]) (i+1:i:is)
-      | otherwise = tM : helper (tM, f1) tM (i+1:i:is)
+chainMinCompositions f = ???
